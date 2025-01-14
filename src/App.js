26140b89357b41d6285c39a232bce43e6dc9fcbc
HammadAamir <hamaamir2001@gmail.com>
2025-01-14 05:11:46 +0100

    Changes in build

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import Login from './component/Login/login'; // Import the updated Login component
+import NavBar from './component/NavBar/NavBar';
+import Sidebar from './component/Sidebar/SideBar';
+import ProductInformation from './component/ProductInformation/ProductInformation';
+import Pdf from './component/Pdf/Pdf';
+import Table from './component/Table/Table';
+import Brandschutzrohr from './component/Brandschutzrohr/Brandschutzrohr';
+import ProductImage from './component/ProductImage/ProductImage';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+  const [selectedComponent, setSelectedComponent] = useState('Home'); // Default selected component
+  const [currentCategory, setCurrentCateogry] = useState(0);
+  useEffect(() => {
+    // Update the page title based on the selected component
+    document.title = `Material Manager - ${selectedComponent}`; 
+  }, [selectedComponent]);
+
+  const handleComponentChange = (newComponent) => {
+    const itemValue = newComponent.split("%")
+    setSelectedComponent(itemValue[0]);
+    setCurrentCateogry(itemValue[1])
+  };
+  useEffect(() => {
+    // Check if user is logged in (e.g., by checking local storage)
+
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoggedIn ? (
+        <> 
+          <NavBar setIsLoggedIn={setIsLoggedIn}/>
+          <div className="container">
+            <Sidebar 
+        selectedComponent={selectedComponent} 
+        onComponentChange={handleComponentChange} />
+            <div className="main-content">
+              <div className="row"
+                    style={{
+                      textAlign: 'center', 
+                      fontSize: '1.2rem', 
+                      fontWeight: 'bold', 
+                      marginBottom: '1rem',
+                      display: 'block'
+                    }}>
+                    {selectedComponent}
+              </div>
+              <div className="row">
+                <ProductInformation categoryID={currentCategory}/>
+                <Pdf categoryID={currentCategory}/>
+              </div>
+              <div className="row">
+                <Table categoryID={currentCategory} />
+                <Brandschutzrohr />
+              </div>
+              <div className="row">
+                <ProductImage categoryID={currentCategory}/>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Login setIsLoggedIn={setIsLoggedIn} /> 
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Table/Table.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import "./table.css";
+
+function Table({ categoryID }) {
+  const [tableData, setTableData] = useState([]); // Holds heading details
+  const [groupedData, setGroupedData] = useState({}); // Groups data by headings
+
+  const fetchHeadingDetails = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/material/heading-details/?categoryID=${categoryID}`);
+    const data = await response.json();
+    console.log(data);
+   
+    const fetchedData = data.map((item) => ({
+      heading: item.heading.headingName,
+      details: { generalInfo: item.generalInfo, DetailedInfo: item.DetailedInfo },
+    }));
+    
+    if (!(typeof data === 'string' || data instanceof String))
+      setTableData(fetchedData);
+    else
+      setTableData(null)
+  };
+
+  useEffect(() => {
+    if (categoryID) fetchHeadingDetails();
+  }, [categoryID]);
+
+  useEffect(() => {
+    const grouped = tableData.reduce((acc, item) => {
+      if (!acc[item.heading]) acc[item.heading] = [];
+      acc[item.heading].push(item.details);
+      return acc;
+    }, {});
+    setGroupedData(grouped);
+    console.log(grouped);
+  }, [tableData]);
+
+  return (
+    <div className="heading-section-container">
+      {Object.keys(groupedData).length > 0 ? (
+        Object.entries(groupedData).map(([heading, details], index) => (
+          <div key={index} className="heading-section-container">
+            <h3>{heading}</h3>
+            <table border={1} className="heading-section-table">
+              <thead>
+                <tr>
+                  <th className="heading-section-table-header">Anforderung</th>
+                  <th className="heading-section-table-header">Eigenschaften</th>
+                </tr>
+              </thead>
+              <tbody>
+                {details.map((detail, detailIndex) => (
+                  <tr key={detailIndex}>
+                    <td>{detail.generalInfo}</td>
+                    <td>{detail.DetailedInfo}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))
+      ) : (
+        
+        <tr>
+        <td colSpan="5" style={{ textAlign: "center" }}>No data available</td>
+      </tr>
+      )}
+    </div>
+  );
+}
+
+export default Table;
